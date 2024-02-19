ab9b16fc7b6778e9c2d9d08ba6cf5005414475af
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2024-02-19 16:10:56 +0100

    Added html and js to single product view

--- js/app.js
+++ js/app.js
@@ -1,99 +1,6 @@
-const productsContainer = document.querySelector("#products-container");
-const filterBtns = document.querySelectorAll(".filterBtn");
-const cartMenuBTN = document.querySelector(".cart-icon-container");
-const cartMenu = document.querySelector("#cartContainer");
-const loader = document.querySelector(".loader");
+import { getProducts, filterProducts, productsContainer, filterBtns, loader } from './utils.js';
 
-async function getProducts() {
-    loader.style.display = "block"
-    try {
-        const response = await fetch("https://v2.api.noroff.dev/rainy-days");
-        const result = await response.json();
-        const productData = result.data;
-        console.log(productData);
-        displayProducts(productData);
-    } catch (error) {
-        console.error('Error fetching products:', error);
-    }
-    loader.style.display = "none"
-}
-
-function displayProducts(jackets) {
-    productsContainer.innerHTML = ``
-    jackets.forEach((jacket) => {
-        if (jacket.onSale) {
-            productsContainer.innerHTML += `
-            <a class="product-card" href="product.html?id=${jacket.id}">
-                <div class="product-image-container">
-                    <img class="product-image" src="${jacket.image.url}">
-                </div>
-                <div class="product-detail-container">
-                    <span class="product-title">${jacket.title}</span>
-                    <div class="price-container">
-                        <span class="product-price-linethrough">${jacket.price}</span>
-                        <span class="product-discount-price">${jacket.discountedPrice}</span>
-                    </div>
-                </div>
-            </a>`
-        }
-        if (!jacket.onSale) {
-            productsContainer.innerHTML += `
-            <a class="product-card" href="product.html?id=${jacket.id}">
-                <div class="product-image-container">
-                    <img class="product-image" src="${jacket.image.url}">
-                </div>
-                <div class="product-detail-container">
-                    <span class="product-title">${jacket.title}</span>
-                    <span class="product-price">${jacket.price}</span>
-                </div>
-            </a>`
-        }
-    })
-}
-
-getProducts()
-
-async function filterProducts(filterParam){
-    loader.style.display = "block"
-    try {
-        const response = await fetch("https://v2.api.noroff.dev/rainy-days");
-        const result = await response.json();
-        const jackets = result.data;
-        productsContainer.innerHTML = ``
-        jackets.forEach((jacket) => {
-            if (jacket.onSale && jacket.gender === filterParam) {
-                productsContainer.innerHTML += `
-            <a class="product-card" href="product.html?id=${jacket.id}">
-                <div class="product-image-container">
-                    <img class="product-image" src="${jacket.image.url}">
-                </div>
-                <div class="product-detail-container">
-                    <span class="product-title">${jacket.title}</span>
-                    <div class="price-container">
-                        <span class="product-price-linethrough">${jacket.price}</span>
-                        <span class="product-discount-price">${jacket.discountedPrice}</span>
-                    </div>
-                </div>
-            </a>`
-            }
-            if (!jacket.onSale && jacket.gender === filterParam) {
-                productsContainer.innerHTML += `
-            <a class="product-card" href="product.html?id=${jacket.id}">
-                <div class="product-image-container">
-                    <img class="product-image" src="${jacket.image.url}">
-                </div>
-                <div class="product-detail-container">
-                    <span class="product-title">${jacket.title}</span>
-                    <span class="product-price">${jacket.price}</span>
-                </div>
-            </a>`
-            }
-        })
-    } catch (error) {
-        console.error('Error filtering products:', error);
-    }
-    loader.style.display = "none"
-}
+getProducts();
 
 filterBtns.forEach((btn) => {
     btn.addEventListener("click", function () {
@@ -105,3 +12,4 @@ filterBtns.forEach((btn) => {
         }
     });
 });
+
